79c583ad70bf61e66a9de5443329e288f49c9032
Martinodark19 <martin.villalobos1906@gmail.com>
2024-10-05 23:09:29 -0300

    cambios config-data

--- microservicio-vuelos/src/main/java/vuelos/microservicio_vuelos/MicroservicioVuelosApplication.java
+++ microservicio-vuelos/src/main/java/vuelos/microservicio_vuelos/MicroservicioVuelosApplication.java
@@ -2,8 +2,10 @@ package vuelos.microservicio_vuelos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicioVuelosApplication {
 
 	public static void main(String[] args) {
