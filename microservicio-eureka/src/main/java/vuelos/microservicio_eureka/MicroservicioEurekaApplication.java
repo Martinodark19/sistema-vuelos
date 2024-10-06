c0797980a22e29892dbe0671e262565811dec62b
Martinodark19 <martin.villalobos1906@gmail.com>
2024-10-05 23:51:26 -0300

    config data last

--- microservicio-eureka/src/main/java/vuelos/microservicio_eureka/MicroservicioEurekaApplication.java
+++ microservicio-eureka/src/main/java/vuelos/microservicio_eureka/MicroservicioEurekaApplication.java
@@ -3,8 +3,8 @@ package vuelos.microservicio_eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 @SpringBootApplication
+//habilitar el servidor de eureka
 @EnableEurekaServer
 public class MicroservicioEurekaApplication 
 {
